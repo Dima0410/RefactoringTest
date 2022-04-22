025da0d62a9bd28981753af72b8f3ac9a0e80594
Dima0410 <73873203+Dima0410@users.noreply.github.com>
2022-04-22 14:53:15 +0500

    Update UserService.cs

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace LegacyApp
 {
@@ -6,24 +6,15 @@ namespace LegacyApp
     {
         public bool AddUser(string firName, string surname, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname))
-            {
-                return false;
-            }
+            if (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname)) return false;
 
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
+            if (!email.Contains("@") && !email.Contains(".")) return false;
 
-            var now = DateTime.Now;
+            DateTime now = DateTime.Now;
             int age = now.Year - dateOfBirth.Year;
             if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
 
-            if (age < 21)
-            {
-                return false;
-            }
+            if (age < 21) return false;
 
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
@@ -49,7 +40,7 @@ namespace LegacyApp
                 using (var userCreditService = new UserCreditServiceClient())
                 {
                     var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
+                    creditLimit *= 2;
                     user.CreditLimit = creditLimit;
                 }
             }
@@ -64,10 +55,7 @@ namespace LegacyApp
                 }
             }
 
-            if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
-                return false;
-            }
+            if (user.HasCreditLimit && user.CreditLimit < 500) return false;
 
             UserDataAccess.AddUser(user);
 
